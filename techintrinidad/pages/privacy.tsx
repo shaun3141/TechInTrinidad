2c9cb57ea378fe97522a01e67162bd8903bc3506
Shaun VanWeelden <shaun.vanweelden@scaleapi.com>
2023-04-10 10:09:25 -0400

    Adding a few more policy terms

--- techintrinidad/pages/privacy.tsx
+++ techintrinidad/pages/privacy.tsx
@@ -11,14 +11,36 @@ export default function Home() {
           <h3>Privacy Policy</h3>
           <Typography>
             Tech in Trinidad (&quot;we&quot;, &quot;us&quot;, or
-            &quot;our&quot;) is committed to protecting your privacy. This
+            &quot;our&quot;) is an online community focused on technology in
+            Trinidad. We are committed to protecting your privacy, and this
             Privacy Policy explains how we collect, use, store, and protect your
             personal information when you interact with our website
-            techintrinidad.com (&quot;Website&quot;) and our services. By using
-            our Website and providing your personal information, you agree to
-            the terms of this Privacy Policy.
+            techintrinidad.com (&quot;Website&quot;) and our services.
           </Typography>
           <br />
+          <Typography>
+            By using our Website and providing your personal information, you
+            agree to the terms of this Privacy Policy.
+          </Typography>
+          <Typography>Definitions</Typography>
+
+          <ul>
+            <li>
+              &quot;Personal Information&quot; - refers to any information that
+              can be used to identify an individual, such as name, email
+              address, or IP address.
+            </li>
+            <li>
+              &quot;Processing&quot; - means any operation performed on personal
+              information, including collection, storage, modification, or
+              deletion.
+            </li>
+            <li>
+              &quot;Data controller&quot; - refers to the entity responsible for
+              determining the purpose and means of processing personal
+              information.
+            </li>
+          </ul>
           <b>Information We Collect</b>
           <Typography>
             We collect the following personal information from our users:
@@ -35,14 +57,26 @@ export default function Home() {
           <b>How We Use Your Information</b>
           <Typography>
             We use the collected information to send newsletters and other
-            communication related to Tech in Trinidad.
+            communication related to Tech in Trinidad. You may opt-out of
+            receiving newsletters by clicking the &quot;unsubscribe&quot; link
+            in any email or by contacting us at&nbsp;
+            <Link href="mailto:techintrinidad@gmail.com">
+              techintrinidad@gmail.com
+            </Link>
+            .
           </Typography>
           <br />
           <b>Data Storage and Security</b>
           <Typography>
-            Your personal information is stored securely in our email provider,
-            SendGrid. We take appropriate measures to protect your information
-            from unauthorized access, alteration, disclosure, or destruction.
+            Your personal information is stored securely with our email service
+            provider, SendGrid. We take appropriate measures to protect your
+            information from unauthorized access, alteration, disclosure, or
+            destruction. You can learn more about SendGrid&apos;s privacy
+            practices by visiting their{" "}
+            <Link href="https://sendgrid.com/policies/security/">
+              Privacy Policy
+            </Link>
+            .
           </Typography>
           <br />
           <b>Data Retention</b>
@@ -86,6 +120,18 @@ export default function Home() {
           <b>Cookies and Tracking Technologies</b>
           <Typography>
             We do not use cookies or tracking technologies on our Website.
+          </Typography>
+          <br />
+          <b>International Data Transfer</b>
+          <Typography>
+            We may process and store your personal information in countries
+            other than your own, including the United States. By using our
+            Website and services, you consent to the transfer of your personal
+            information to countries outside your country of residence, which
+            may have different data protection laws than those in your country.
+            We take appropriate measures to ensure that your personal
+            information is treated securely and in accordance with this Privacy
+            Policy and applicable data protection laws.
           </Typography>
           <br />
           <b>Compliance with GDPR and CCPA</b>
